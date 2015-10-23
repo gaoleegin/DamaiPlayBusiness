e831c6ac122d1b554a0e8fd2ff6de07787ef2a6d
ershixiong <lijungao1999@163.com>
2015-10-23 08:29:09 +0800

    修改登录界面

--- DamaiPlayBusinessPhone/DamaiPlayBusinessPhone/Classes/Module/UserLogin/DMUserLoginViewController.swift
+++ DamaiPlayBusinessPhone/DamaiPlayBusinessPhone/Classes/Module/UserLogin/DMUserLoginViewController.swift
@@ -7,7 +7,8 @@
 //
 
 import UIKit
-
+import Alamofire
+import SVProgressHUD
 
 class DMUserLoginViewController: UITableViewController {
     
@@ -160,55 +161,9 @@ class DMUserLoginViewController: UITableViewController {
         let userName:String = (self.firstTextField?.text)!
         let passWord:String = (self.secondTextField?.text)!
         
-        
+        SVProgressHUD.showWithStatus("正在加载")
         
         print("用户名：\(userName)" + "密码：\(passWord)")
         
     }
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
