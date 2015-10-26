06ebb927abff0402972e910074461eeb9b78b744
ershixiong <lijungao1999@163.com>
2015-10-26 17:29:43 +0800

    首页的网络接口的调试

---
+++ DamaiPlayBusinessPhone/DamaiPlayBusinessPhone/Classes/Module/ProjectList/DMProjectListInfo.swift
@@ -0,0 +1,13 @@
+//
+//  DMProjectListInfo.swift
+//  DamaiPlayBusinessPhone
+//
+//  Created by 高李军 on 15/10/26.
+//  Copyright © 2015年 DamaiPlayBusinessPhone. All rights reserved.
+//
+
+import UIKit
+
+class DMProjectListInfo: NSObject {
+
+}
